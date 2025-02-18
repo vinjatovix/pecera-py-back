de408cda2ccde4d68544851d4717c83f764b0d7d
vinjadevix <vinjatovix@gmail.com>
2025-02-18 20:39:08 +0100

    feat: grab listening port from .env

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
+import uvicorn
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/api/v1/health/http")
 def get_health():
     return {"status": "OK"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
